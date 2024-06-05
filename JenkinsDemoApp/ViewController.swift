20603e3d52ecc78399eca47ddaaae3533c26fb17
Mert Gökduman <mert.gokduman@nesine.com>
2024-06-05 22:04:09 +0300

    TrialView is added

--- JenkinsDemoApp/ViewController.swift
+++ JenkinsDemoApp/ViewController.swift
@@ -9,6 +9,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private lazy var trialView: UIView = {
+        let view = UIView(frame: .zero)
+        return view
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
