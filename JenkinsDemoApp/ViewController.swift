0911baef40bc308c87ce8c7200a32cf0eaa9c8c1
Mert Gökduman <mert.gokduman@nesine.com>
2024-06-05 21:44:15 +0300

    Initial Commit

---
+++ JenkinsDemoApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JenkinsDemoApp
+//
+//  Created by Mert Gökduman on 5.06.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
